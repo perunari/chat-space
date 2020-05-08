81774dc6c3ca646246b3a696cd1314a116814b2a
perunari <per.roc.ski@gmail.com>
2020-05-08 11:16:01 +0900

    Revert "メッセージ非同期実装"
    This reverts commit c2e23a32914868f67f98f215c465998fe02ff196.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="box" data-message-id=${message.id}>
+      `<div class="box">
          <div class="box__data">
            <div class="name">
              ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
      return html;
    } else {
      var html =
-      `<div class="box" data-message-id=${message.id}>
+      `<div class="box">
          <div class="box__data">
            <div class="name">
              ${message.user_name}
@@ -62,30 +62,4 @@ $('#new_message').on('submit', function(e){
     alert("メッセージ送信に失敗しました");
 });
 })
-
-  var reloadMessages = function() {
-    var last_message_id = $('.message:last').data("message-id");
-    $.ajax({
-    url: "api/messages",
-    type: 'get',
-    dataType: 'json',
-    data: {id: last_message_id}
-  })
-    .done(function(messages) {
-      if (messages.length !== 0) {
-      var insertHTML = '';
-      $.each(messages, function(i, message) {
-        insertHTML += buildHTML(message)
-      });
-      $('.messages').append(insertHTML);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      }
-    })
-    .fail(function() {
-      alert('更新に失敗しました');
-    });
-  };
-  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 7000);
-  }
 });
